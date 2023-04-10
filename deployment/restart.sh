4b208968467b38449a20366a2b3a7c9039bf981e
iwatsukikevin <111888968+iwatsukikevin@users.noreply.github.com>
2023-04-10 13:56:54 -0400

    Update restart.sh

--- deployment/restart.sh
+++ deployment/restart.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 docker stop kev_site
 docker rm kev_site
+docker ps -a
 docker pull iwatsukikevin/kmirepo:latest
+docker ps -a
 docker run -dp 80:80 --name kev_site iwatsukikevin/kmirepo:latest
+docker ps -a
